181685d6ed6a402ab1efa4ca1bb05e3c05724958
fjebaker <fergusbkr@gmail.com>
2021-12-23 00:17:21 +0100

    replaced bad version

--- day22/day22.jl
+++ day22/day22.jl
@@ -1,15 +1,26 @@
+using BenchmarkTools
+
 struct Region
-    state::Int
-    x::Tuple{Int, Int}
-    y::Tuple{Int, Int}
-    z::Tuple{Int, Int}
+    state::Bool
+    x::Tuple{Int,Int}
+    y::Tuple{Int,Int}
+    z::Tuple{Int,Int}
 end
 
+const NULL_REGION = Region(0, (0,0), (0,0), (0,0))
+
+volume(r::Region) = abs(
+    (r.x[2] + 1 - r.x[1]) * (r.y[2] + 1 - r.y[1]) * (r.z[2] + 1 - r.z[1])
+)
+
+getoverlaps(r, regions) = unique!([
+    i for i in (r ∩ other for other in regions) if i != NULL_REGION
+])
+
 function makeregion(match)
-    state = match[1] == "on" ? 1 : 0
     ranges = parse.(Int, match.captures[2:end])
     Region(
-        state,
+        match[1] == "on",
         (ranges[1], ranges[2]),
         (ranges[3], ranges[4]),
         (ranges[5], ranges[6])
@@ -24,75 +35,77 @@ function parse_input(path)
     makeregion.(matches)
 end
 
-
-
-function get_overlap(t1, t2)
-    if overlaps(t1, t2)
-        high = min(t2[2], t1[2])
-        low = max(t2[1], t1[1])
+function getoverlap(t1, t2)
+    low = max(t2[1], t1[1])
+    high = min(t2[2], t1[2])
+    if low ≤ high
         return (low, high)
     end
     (0, 0)
 end
 
+getoverlap(r1::Region, r2::Region) = (
+    getoverlap(r1.x, r2.x), getoverlap(r1.y, r2.y), getoverlap(r1.z, r2.z)
+)
+
 function Base.intersect(r1::Region, r2::Region)
-    x = get_overlap(r1.x, r2.x)
-    y = get_overlap(r1.y, r2.y)
-    z = get_overlap(r1.z, r2.z)
+    x, y, z = getoverlap(r1, r2)
     # if it doesn't overlap, or they are both "off" states
-    if x == (0, 0) || y == (0, 0) || z == (0, 0) || r1.state == r2.state == 0
+    if x == (0, 0) || y == (0, 0) || z == (0, 0)
         return NULL_REGION
     end
     Region(r2.state, x, y, z)
 end
 
-function addoverlap!(ons, toremove, overlaps)
-    region = first(overlaps)
-    for other in overlaps[2:end]
-        overlap = region ∩ other
-        if overlap != NULL_REGION
-end
-
-function calculate(regions)
-    toremove = Region[]
-    ons = Region[]
-
-    for (i, r) in enumerate(regions)
-        ons_copy = copy(ons)
-        toremove_copy = copy(toremove)
-
-        # check if overlaps any of the previous
-        overlaps = Region[]
-        for other in ons_copy
-            overlap = r ∩ other
-            if overlap != NULL_REGION
-                push!(overlaps, overlap)
-            end
-        end
-        addoverlap!(ons, toremove, overlaps)
-
-        if r.state == 1
-            println("adding $r to ons")
-            push!(ons, r)
-            println(" => $ons")
-        end
-
-        println()
+function total_volume(regions)
+    if isempty(regions)
+        return 0
     end
 
-    #sum(volume, ons)
-    toremove, ons
+    region = popfirst!(regions)
+
+    overlaps = getoverlaps(region, regions)
+
+    return volume(region) + total_volume(regions) - total_volume(overlaps)
 end
 
 
-regions = parse_input("day22/test_large.txt")
+function countlit(regions)
+    if isempty(regions)
+        return 0
+    end
 
-r = Region(1, (0, 5), (0, 5), (0, 5))
-r2 = Region(1, (0, 6), (0, 5), (0, 5))
+    region = popfirst!(regions)
 
-println("*"^50)
-remove, ons = calculate((r, r, r))
+    if region.state == 0
+        return countlit(regions)
+    end
 
-length(remove), length(ons)
+    overlaps = getoverlaps(region, regions)
 
-#calculate(regions)
+    return volume(region) + countlit(regions) - total_volume(overlaps)
+end
+
+function part1(regions)
+    mask = Region(1, (-50, 50), (-50, 50), (-50, 50))
+    subregions = getoverlaps(mask, regions)
+    countlit(subregions)
+end
+
+function part2(regions)
+    countlit(regions)
+end
+
+
+
+regions = parse_input("day22/input.txt")
+
+pt1 = @btime part1(rs) setup = (rs = deepcopy($regions))
+# 3.431 ms (68216 allocations: 13.34 MiB)
+
+pt2 = @btime part2(rs) setup = (rs = deepcopy($regions))
+# 1.341 ms (24308 allocations: 4.75 MiB)
+
+
+println("Part1: $pt1")
+println("Part2: $pt2")
